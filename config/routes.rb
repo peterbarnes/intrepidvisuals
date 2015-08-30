4ef978bd0d4c162358f4baba4cb55dbc0ea2cca8
peterbarnes <barnespeter213@gmail.com>
2015-08-30 04:52:55 -0500

    Designing, error pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   root 'static#home'
   match '/events',     to: 'static#events',    via: 'get'
   match '/portraits',  to: 'static#portraits', via: 'get'
-  match '/landscape',  to: 'static#landscape', via: 'get'
   match '/video',      to: 'static#video',     via: 'get'
   match '/seniors',    to: 'static#seniors',   via: 'get'
   match '/about',      to: 'static#about',     via: 'get'
